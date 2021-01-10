d996b5d84916ad51933d6ef0d53de8a0e7f31ec6
takuro.moto <kpdvox8370@yahoo.co.jp>
2021-01-10 11:50:10 +0000

    edit UI TOPpage

--- resources/views/giving_users/giving_users.blade.php
+++ resources/views/giving_users/giving_users.blade.php
@@ -1,55 +1,70 @@
 @if (count($giving_users) > 0)
-    <ul class="list-unstyled">
-        @foreach ($giving_users as $giving_user)
-            <li class="media mb-3">
-                {{-- 投稿の所有者のメールアドレスをもとにGravatarを取得して表示 --}}
-                <img class="mr-2 rounded" src="{{ Gravatar::get($giving_user->user->email, ['size' => 50]) }}" alt="">
-                <div class="media-body">
-                    <div>
-                        {{-- 投稿の所有者のユーザ詳細ページへのリンク --}}
-                        {!! link_to_route('users.show', $giving_user->user->name, ['user' => $giving_user->user->id]) !!}
-                        <span class="text-muted">posted at {{ $giving_user->created_at }}</span>
-                    </div>
-                    <div>
-                        {{-- 投稿内容 --}}
-                        <p class="mb-0">名前：{!! nl2br(e($giving_user->name)) !!}</p>
-                        <p class="mb-0">関係性：{!! nl2br(e($giving_user->relation)) !!}</p>
-                            @foreach ($giving_user->anniversaries as $anniversary)
-                        <p class="mb-0">お祝い：{!! nl2br(e($anniversary->anniversary)) !!}</p>
-                        <p class="mb-0">日程：{!! nl2br(e($anniversary->day->format('n月j日') )) !!}</p>
-                            <div>
-                            @if (Auth::id() == $giving_user->user_id)
-                                <button class="btn btn-default btn-sm">{!! link_to_route('presents.create', 'プレゼントを登録する', ['id' => $anniversary->id], []) !!}</button>
-                                {{-- 投稿削除ボタンのフォーム --}}
-                                 @foreach ($anniversary->presents as $present)
-                                <p class="mb-0">プレゼント：{!! nl2br(e($present->present)) !!}</p>
-                                <p class="mb-0">あげた年：{!! nl2br(e($present->year)) !!}</p>
-                                {!! Form::open(['route' => ['presents.destroy', $present->id], 'method' => 'delete']) !!}
-                                    {!! Form::submit('プレゼントを消す', ['class' => 'btn btn-danger btn-sm']) !!}
-                                {!! Form::close() !!}
-                                @endforeach
-                            @endif
-                            </div>
-                        {!! Form::open(['route' => ['anniversaries.destroy', $anniversary->id], 'method' => 'delete']) !!}
-                                {!! Form::submit('お祝いを消す', ['class' => 'btn btn-danger btn-sm']) !!}
-                        {!! Form::close() !!}
-                            @endforeach
-                    </div>
-
-                    <div>
-                        @if (Auth::id() == $giving_user->user_id)
-                            <button class="btn btn-default btn-sm">{!! link_to_route('anniversaries.create', 'お祝いを登録する', ['id' => $giving_user->id], []) !!}</button>
-                            {{-- 投稿削除ボタンのフォーム --}}
-                            
-                            {!! Form::open(['route' => ['giving_users.destroy', $giving_user->id], 'method' => 'delete']) !!}
-                                {!! Form::submit('送りたい相手を消す', ['class' => 'btn btn-danger btn-sm']) !!}
-                            {!! Form::close() !!}
-                        @endif
-                    </div>
-                </div>
-            </li>
-        @endforeach
-    </ul>
+    @foreach ($giving_users as $giving_user)
+    @foreach ($giving_user->anniversaries as $anniversary)
+    @foreach ($anniversary->presents as $present)
+    <section>
+        <div class="d-flex justify-content-center align-items-center mb-6">
+            <div>
+                <ul class="list-unstyled">
+                    <li class="media mb-3">
+                        <div class="media-body">
+                            <table class="table table-borderless">
+                                <thead>
+                                    <tr>
+                                        <th scope="col" colspan="2">投稿者</th>
+                                        <th scope="col">名前</th>
+                                        <th scope="col">関係性</th>
+                                        <th scope="col">お祝い</th>
+                                        <th scope="col">日程</th>
+                                        <th scope="col">プレゼント</th>
+                                        <th scope="col">あげた年</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th colspan="2">{!! (e($giving_user->user->name)) !!}</th>
+                                        <td>{!! (e($giving_user->name)) !!}</td>
+                                        <td>{!! (e($giving_user->relation)) !!}</td>
+                                        <td>{!! (e($anniversary->anniversary)) !!}</td>
+                                        <td>{!! (e($anniversary->day->format('n月j日') )) !!}</td>
+                                        <td>{!! (e($present->present)) !!}</td>
+                                        <td>{!! (e($present->year)) !!}</td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="2">
+                                            <img class="mr-2 rounded" src="{{ Gravatar::get($giving_user->user->email, ['size' => 50]) }}" alt="">
+                                        </td>
+                                        @if (Auth::id() == $giving_user->user_id)
+                                            <td colspan="2">
+                                                {{-- 削除ボタンのフォーム --}}
+                                                {!! Form::open(['route' => ['giving_users.destroy', $giving_user->id], 'method' => 'delete']) !!}
+                                                    {!! Form::submit('送りたい相手を消す', ['class' => 'btn btn-danger btn-sm']) !!}
+                                                {!! Form::close() !!}
+                                            </td>
+                                            <td colspan="2">
+                                                {!! Form::open(['route' => ['anniversaries.destroy', $anniversary->id], 'method' => 'delete']) !!}
+                                                {!! Form::submit('お祝いを消す', ['class' => 'btn btn-danger btn-sm']) !!}
+                                                {!! Form::close() !!}
+                                            </td>
+                                            <td colspan="2">
+                                                {!! Form::open(['route' => ['presents.destroy', $present->id], 'method' => 'delete']) !!}
+                                                {!! Form::submit('プレゼントを消す', ['class' => 'btn btn-danger btn-sm']) !!}
+                                                {!! Form::close() !!}
+                                            </td>
+                                        @endif
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </li>
+           
+                </ul>
+            </div>
+    </div>
+    </section>
+    @endforeach
+    @endforeach
+    @endforeach
     {{-- ページネーションのリンク --}}
     {{ $giving_users->links() }}
 @endif

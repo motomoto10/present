66162bd6751a9a58d4978ec4975e791f3dff7f20
takuro.moto <kpdvox8370@yahoo.co.jp>
2021-01-31 15:18:25 +0000

    navarea div

--- resources/views/users/navtab.blade.php
+++ resources/views/users/navtab.blade.php
@@ -1,26 +1,18 @@
 <div class="col-sm-12">
-    <ul class="nav nav-justified mb-3">
+    <div class ="row">
         {{-- いいね一覧タブ --}}
-        <li class="nav-item">
             
-        <div class="col-sm">{!! link_to_route('presnts.favorite_present', 'いいね一覧', ['id' => $user->id], ['class' => 'btn-full-green']) !!}</div>
+        <div class="col-sm-3 text-center">{!! link_to_route('presnts.favorite_present', 'いいね一覧', ['id' => $user->id], ['class' => 'btn-full-green']) !!}</div>
 
-        </li>
         {{-- giving_user一覧タブ --}}
-        <li class="nav-item">
             
-        <div class="col-sm">{!! link_to_route('users.giving_users', 'プレゼントをあげる人一覧', ['id' => $user->id], ['class' => 'btn-full-blue']) !!}</div>
+        <div class="col-sm-3 text-center">{!! link_to_route('users.giving_users', 'プレゼントをあげる人一覧', ['id' => $user->id], ['class' => 'btn-full-blue']) !!}</div>
 
-        </li>
         {{-- anniversary一覧タブ --}}
-        <li class="nav-item">
             
-        <div class="col-sm">{!! link_to_route('users.anniversaries', 'お祝い一覧', ['id' => $user->id], ['class' => 'btn-full-pink']) !!}</div>
+        <div class="col-sm-3 text-center">{!! link_to_route('users.anniversaries', 'お祝い一覧', ['id' => $user->id], ['class' => 'btn-full-pink']) !!}</div>
 
-        </li>
         {{-- present覧タブ --}}
-        <li class="nav-item">
-            <div class="col-sm">{!! link_to_route('users.presents', 'プレゼント一覧', ['id' => $user->id], ['class' => 'btn-full-yellow']) !!}</div>
-        </li>
-    </ul>
+        <div class="col-sm-3 text-center">{!! link_to_route('users.presents', 'プレゼント一覧', ['id' => $user->id], ['class' => 'btn-full-yellow']) !!}</div>
+    </div>
 </div>

26ca6541b1837548d5fbacac52b2f30fe250af14
takuro.moto <kpdvox8370@yahoo.co.jp>
2021-01-31 15:02:05 +0000

    humberger menu

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -3,8 +3,8 @@
         {{-- トップページへのリンク --}}
         <a class="navbar-brand navbar-font" href="/">Present</a>
 
-        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
-            <span class="navbar-toggler-icon"></span>
+        <button type="button" class="navbar-toggler navbar-font" data-toggle="collapse" data-target="#nav-bar">
+            <span class="navbar-toggler-icon"><i class="fas fa-bars"></i></span>
         </button>
 
         <div class="collapse navbar-collapse" id="nav-bar">
